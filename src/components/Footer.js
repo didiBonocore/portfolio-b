6f2f887d6247b4a8e37c87e8d4a86141f7c101f5
Eugenia Bonocore <eugenia@prospectpath.com>
2022-01-30 12:01:34 +0000

    Add relationship attribute to link tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -70,7 +70,7 @@ export default function Footer() {
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
         {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} target={"_blank"} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} target="_blank" rel="noreferrer" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
